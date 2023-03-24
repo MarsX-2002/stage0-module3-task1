973c8b6b03f6f5ec863d6c22b485c0343868fc22
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-24 17:26:12 +0500

    logger

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -1,11 +1,8 @@
 package lang.print.gaps.task1;
 
-import java.util.logging.Logger;
-
 public class PrintWordsApp {
-    private static final Logger LOGGER = Logger.getLogger(PrintWordsApp.class.getName());
-
     public static void main(String[] args) {
-        LOGGER.info("java\nis\nsmth");
+        System.out.println("java\nis\nsmth");
+
     }
 }
